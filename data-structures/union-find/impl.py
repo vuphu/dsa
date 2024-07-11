10f067f0af26982d1673e6c3e87746b3385f2134
vuphu <vuphu.itus@gmail.com>
2024-07-11 08:30:11 +0700

    feat(ds): add union find

---
+++ data-structures/union-find/impl.py
@@ -0,0 +1,21 @@
+class UnionFind:
+    def __init__(self, n: int) -> None:
+        self.parents = list(range(n))
+        
+    def find_parent(self, u: int) -> int:
+        if self.parents[u] != u:
+            self.parents[u] = self.find_parent(self.parents[u])
+        return self.parents[u]
+    
+    def union(self, u: int, v: int) -> None:
+        pu = self.find_parent(u)
+        pv = self.find_parent(v)
+        if pu != pv:
+            self.parents[pu] = pv
+
+if __name__ == "__main__":
+    union_find = UnionFind(10)
+    union_find.union(1, 2)
+    union_find.union(1, 3)
+    union_find.union(2, 4)
+    print(union_find.find_parent(3) == union_find.find_parent(4))
