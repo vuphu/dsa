a4b28d6226ce68c243a79eb44262c6e2b2df7f0d
vuphu <vuphu.itus@gmail.com>
2024-07-27 12:13:28 +0700

    feat(ds): add trie impl

---
+++ data-structures/trie/impl.py
@@ -0,0 +1,48 @@
+import collections
+from typing import List, Optional
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.is_end = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def build(self, words: List[str]) -> None:
+        for word in words:
+            node = self.root
+            for char in word:
+                if char not in node.children:
+                    node.children[char] = TrieNode()
+                node = node.children[char]
+            node.is_end = True
+
+    def __find_node(self, word: str) -> Optional[TrieNode]:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return None
+            node = node.children[char]
+        return node
+
+    def exists(self, word: str) -> bool:
+        node = self.__find_node(word)
+        return node is not None and node.is_end
+
+    def starts_with(self, prefix: str) -> bool:
+        node = self.__find_node(prefix)
+        return node is not None
+
+
+if __name__ == "__main__":
+    words = ["dsa", "python", "data-structure", "trie", "impl"]
+    trie = Trie()
+
+    trie.build(words)
+    print(trie.exists("python"))
+    print(trie.exists("algorithm"))
+    print(trie.starts_with("imp"))
