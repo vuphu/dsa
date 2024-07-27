f431e3efd275ad9a1edb097b655c90a2635d607c
vuphu <vuphu.itus@gmail.com>
2024-07-27 12:32:05 +0700

    feat(ds): add sparse table

---
+++ data-structures/sparse-table/impl.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class SparseTable:
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.n = len(nums)
+        self.log = [0] * (self.n + 1)
+        self.build_log()
+        self.sparse_table = self.build_sparse_table()
+
+    def build_log(self):
+        for i in range(2, self.n + 1):
+            self.log[i] = self.log[i // 2] + 1
+
+    def build_sparse_table(self):
+        k = self.log[self.n] + 1
+        st = [[0] * k for _ in range(self.n)]
+        
+        for i in range(self.n):
+            st[i][0] = self.nums[i]
+        
+        j = 1
+        while (1 << j) <= self.n:
+            i = 0
+            while (i + (1 << j) - 1) < self.n:
+                st[i][j] = min(st[i][j-1], st[i + (1 << (j-1))][j-1])
+                i += 1
+            j += 1
+        
+        return st
+
+    def range_min_query(self, start_at, end_at):
+        j = self.log[end_at - start_at + 1]
+        return min(self.sparse_table[start_at][j], self.sparse_table[end_at - (1 << j) + 1][j])
+    
+if __name__ == "__main__":
+    nums = [1, 3, 2, 7, 9, 11, 3, 5, 6, 4, 8]
+    sparse_table = SparseTable(nums)   
+    print(sparse_table.range_min_query(0, 5))
+    print(sparse_table.range_min_query(3, 8))
