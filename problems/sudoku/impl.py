977b8d27c747dbf3077a60456c35b732000ba922
vuphu <vuphu.itus@gmail.com>
2024-08-18 00:02:19 +0700

    feat(problems): solve sodoku problem

--- problems/sudoku/impl.py
+++ problems/sudoku/impl.py
@@ -1,3 +1,4 @@
+from math import sqrt
 from typing import List
 
 
@@ -8,7 +9,43 @@ def print_board(board: List[List[str]]):
 
 
 def solve_sudoku(board: List[List[str]]) -> None:
-    pass
+    n = len(board)
+    n_box = int(sqrt(n))
+
+    def get_start_box(r: int, c: int) -> List[int]:
+        return [
+            r // n_box * n_box,
+            c // n_box * n_box
+        ]
+
+    def can_put(r: int, c: int, num: int) -> bool:
+        for i in range(n):
+            values = [board[r][i], board[i][c]]
+            if str(num) in values:
+                return False
+
+        r_start_box, c_start_box = get_start_box(r, c)
+        for i in range(n_box):
+            for j in range(n_box):
+                if str(num) == board[r_start_box + i][c_start_box + j]:
+                    return False
+
+        return True
+
+    def attempt() -> bool:
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == '.':
+                    for k in range(1, n + 1):
+                        if can_put(i, j, k):
+                            board[i][j] = str(k)
+                            if attempt():
+                                return True
+                            board[i][j] = '.'
+                    return False
+        return True
+
+    attempt()
 
 
 if __name__ == "__main__":
@@ -23,7 +60,7 @@ if __name__ == "__main__":
         [".", ".", ".", "4", "1", "9", ".", ".", "5"],
         [".", ".", ".", ".", "8", ".", ".", "7", "9"]
     ]
-    
+
     print_board(board)
     solve_sudoku(board)
     print_board(board)
