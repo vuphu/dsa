22de3c38da550d7346e451011cc376c99d20a63a
Phuong Vo <vuphu.itus@gmail.com>
2024-09-06 09:50:25 +0700

    feat(algo): add meet-in-the-middle algorithm

--- algorithms/meet-in-the-middle/impl.py
+++ algorithms/meet-in-the-middle/impl.py
@@ -1,2 +1,29 @@
+import collections
+from typing import List
+
+
+def find_subset_sums(nums: List[int]) -> List[int]:
+    ans = []
+
+    def attempt(i: int, s: int) -> None:
+        if i == len(nums):
+            ans.append(s)
+            return
+        attempt(i + 1, s)
+        attempt(i + 1, s + nums[i])
+
+    attempt(0, 0)
+    return ans
+
+
+def count_target_subsets(nums: List[int], target: int) -> int:
+    n = len(nums)
+    front_sums = find_subset_sums(nums[:n // 2])
+    back_sums = find_subset_sums(nums[n // 2:])
+    back_sum_counter = collections.Counter(back_sums)
+    return sum([back_sum_counter[target - s] for s in front_sums])
+
+
 if __name__ == "__main__":
-    pass
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert count_target_subsets(nums, 10) == 10
