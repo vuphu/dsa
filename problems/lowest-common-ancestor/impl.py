3ee9144ff471182875dbcb56eaea9b174c09a66c
Phuong Vo <vuphu.itus@gmail.com>
2024-09-09 04:22:51 +0700

    feat(problems): add lowest common ancestor problem

--- problems/lowest-common-ancestor/impl.py
+++ problems/lowest-common-ancestor/impl.py
@@ -1,2 +1,43 @@
+from os import access
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, value: int, left: Optional['TreeNode'] = None, right: Optional['TreeNode'] = None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def lowest_common_ancestor(root: TreeNode, first_value: int, second_value: int) -> TreeNode:
+    def dfs(node: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not node:
+            return None
+
+        if node.value in [first_value, second_value]:
+            return node
+
+        found_left = dfs(node.left)
+        found_right = dfs(node.right)
+
+        if found_left and found_right:
+            return node
+        else:
+            return found_left or found_right
+
+    return dfs(root)
+
+
 if __name__ == "__main__":
-    pass
+    root = TreeNode(
+        value=1,
+        left=TreeNode(value=2),
+        right=TreeNode(
+            value=3,
+            left=TreeNode(value=4),
+            right=TreeNode(value=5)
+        )
+    )
+
+    ancestor = lowest_common_ancestor(root, 2, 5)
+    assert ancestor == root
