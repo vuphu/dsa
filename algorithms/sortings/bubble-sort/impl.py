526b2bd88d9bef14dbb597cc16a614741cdb1807
vuphu <vuphu.itus@gmail.com>
2025-01-04 00:32:16 +0700

    feat(algo): add bubble sort

--- algorithms/sortings/bubble-sort/impl.py
+++ algorithms/sortings/bubble-sort/impl.py
@@ -2,7 +2,14 @@ from typing import List
 
 
 def bubble_sort(nums: List[int]) -> List[int]:
-    pass
+    n = len(nums)
+
+    for i in range(n):
+        for j in range(n - i - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+    return nums
 
 if __name__ == "__main__":
     nums = [4, 2, 1, 3, 5]
