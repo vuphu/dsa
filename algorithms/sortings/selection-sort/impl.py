b2f8877d25e73f023375a4da31ad41792fd0c5fe
vuphu <vuphu.itus@gmail.com>
2025-01-05 00:18:01 +0700

    feat(algo): add selection sort

---
+++ algorithms/sortings/selection-sort/impl.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def selection_sort(nums: List[int]) -> List[int]:
+    n = len(nums)
+
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if nums[j] < nums[min_index]:
+                min_index = j
+        nums[i], nums[min_index] = nums[min_index], nums[i]
+
+    return nums
+
+
+if __name__ == "__main__":
+    nums = [4, 2, 1, 3, 5]
+    assert selection_sort(nums) == sorted(nums)
