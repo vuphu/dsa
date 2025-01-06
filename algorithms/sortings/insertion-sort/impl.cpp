d8d63625dc229c983000c8669c12f6b958f6a327
vuphu <vuphu.itus@gmail.com>
2025-01-07 00:05:44 +0700

    feat(algo): add insertion sort

--- algorithms/sortings/insertion-sort/impl.cpp
+++ algorithms/sortings/insertion-sort/impl.cpp
@@ -3,6 +3,18 @@
 #include <cassert>
 
 std::vector<int> insertion_sort(std::vector<int> &nums) {
+    int n = nums.size();
+
+    for (int i = 1; i < n; i++) {
+        int value = nums[i];
+        int j = i - 1;
+        while (j >= 0 && nums[j] > value) {
+            nums[j + 1] = nums[j];
+            j -= 1;
+        }
+        nums[j + 1] = value;
+    }
+
     return nums;
 }
 
--- algorithms/sortings/insertion-sort/impl.py
+++ algorithms/sortings/insertion-sort/impl.py
@@ -2,7 +2,18 @@ from typing import List
 
 
 def insertion_sort(nums: List[int]) -> List[int]:
-    pass
+    n = len(nums)
+
+    for i in range(1, n):
+        value = nums[i]
+        j = i - 1
+        while j >= 0 and nums[j] > value:
+            nums[j + 1] = nums[j]
+            j -= 1
+        nums[j + 1] = value
+
+    return nums
+
 
 
 if __name__ == "__main__":
