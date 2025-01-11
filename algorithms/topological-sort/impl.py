e6872379270b8690c151ab480f86d1373097fccf
vuphu <vuphu.itus@gmail.com>
2025-01-11 12:37:58 +0700

    feat(algo): add topological sort

--- algorithms/topological-sort/impl.py
+++ algorithms/topological-sort/impl.py
@@ -1,11 +1,30 @@
+import collections
 from typing import List
 
 
 def topological_sort(n: int, edges: List[List[int]]) -> List[int]:
-    pass
+    graph = collections.defaultdict(list)
+    for u, v in edges:
+        graph[u].append(v)
+
+    visited = [False] * n
+    stack = []
+
+    def dfs(node):
+        visited[node] = True
+        for neighbor in graph[node]:
+            if not visited[neighbor]:
+                dfs(neighbor)
+        stack.append(node)
+
+    for v in range(n):
+        if not visited[v]:
+            dfs(v)
+
+    return stack[::-1]
 
 
 if __name__ == "__main__":
     n = 6
-    edges = [[1, 2], [2, 3], [3, 4], [4, 5]]
-    assert topological_sort(n, edges) == [1, 2, 3, 4, 5]
+    edges = [[0, 1], [0, 2], [1, 2], [2, 3], [3, 4], [4, 5]]
+    assert topological_sort(n, edges) == [0, 1, 2, 3, 4, 5]
