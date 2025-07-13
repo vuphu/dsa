9135c812bb7f94aa963aae805b4f7f7f98b16635
vuphu <vuphu.itus@gmail.com>
2025-07-13 23:28:08 +0700

    feat(ds): union find rust implementation

---
+++ data-structures/union-find/impl.rs
@@ -0,0 +1,35 @@
+struct UnionFind {
+    parents: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        Self {
+            parents: (0..n).collect::<Vec<_>>(),
+        }
+    }
+
+    fn find_parent(&mut self, u: usize) -> usize {
+        if self.parents[u] != u {
+            self.parents[u] = self.find_parent(self.parents[u]);
+        }
+        return self.parents[u];
+    }
+
+    fn unite(&mut self, u: usize, v: usize) {
+        let pu = self.find_parent(u);
+        let pv = self.find_parent(v);
+        if pu != pv {
+            self.parents[pu] = pv;
+        }
+    }
+}
+
+fn main() {
+    let mut union_find = UnionFind::new(10);
+    union_find.unite(1, 2);
+    union_find.unite(1, 3);
+    union_find.unite(2, 4);
+
+    assert!(union_find.find_parent(3) == union_find.find_parent(4));
+}
