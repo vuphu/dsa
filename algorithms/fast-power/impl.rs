6b8610b727e0e758fe37a5e2be545594675ff1d8
vuphu <vuphu.itus@gmail.com>
2025-09-30 23:19:51 +0700

    feat(algo): update fast power rust implementation

--- algorithms/fast-power/impl.rs
+++ algorithms/fast-power/impl.rs
@@ -1,20 +1,20 @@
 const MOD: i64 = 1_000_000_007;
 
-fn fast_power(base: i32, power: i32, modulo: i64) -> i32 {
+fn fast_power(base: i32, power: i32) -> i32 {
     if power == 0 {
         return 1;
     }
 
-    let half = fast_power(base, power / 2, modulo) as i64;
-    let mut ans = (half * half) % modulo;
+    let half = fast_power(base, power / 2) as i64;
+    let mut ans = (half * half) % MOD;
     if power % 2 == 1 {
-        ans = (ans * (base as i64)) % modulo
+        ans = (ans * (base as i64)) % MOD
     }
 
     ans as i32
 }
 
 fn main() {
-    assert!(fast_power(2, 3, MOD) == 8);
-    assert!(fast_power(256, 1024, MOD) == 112754241)
+    assert!(fast_power(2, 3) == 8);
+    assert!(fast_power(256, 1024) == 112754241)
 }
